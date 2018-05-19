5db00f9d1f7f36dd9676b71cbe4d251c0bca5ab3
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-05-19 23:03:59 +0800

    Add files via upload

---
+++ functools_itertools_operator.py
@@ -0,0 +1,100 @@
+import functools,itertools,operator
+
+
+#cmp_to_key:比较函数
+key1=functools.cmp_to_key(lambda x,y:x%3-y%3)
+key2=functools.cmp_to_key(lambda x,y:d[x]-d[y])
+a=[1,8,4,6,2]
+print(sorted(a,key=key1))
+#=>[6, 1, 4, 8, 2]
+d={'a':3,'b':1,'c':6}
+print(sorted(d,key=key2))
+#=>['b', 'a', 'c']
+
+
+#partial:冻结部分函数位置函数
+def say(a,b):
+    print(a,b)
+nya=functools.partial(say,b='nya!!!')
+nya('good')
+#=>good nya!!!
+
+
+#reduce:两个参数的function从左至右依次作用于序列中的项，减少序列为单个值
+nya=functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5])
+print(nya)
+#=>15
+
+
+#count:count(start=0, step=1) 会返回一个无限的整数iterator，每次增加1。可以选择提供起始编号，默认为0。
+for i in zip(itertools.count(start=0,step=1), ['a', 'b', 'c']):
+    print(i, end='#')
+#=>(0, 'a')#(1, 'b')#(2, 'c')#
+
+
+#cycle:cycle(iterable) 会把传入的一个序列无限重复下去，不过可以提供第二个参数就可以制定重复次数。
+for i in zip(range(7), itertools.cycle(['a', 'b', 'c'])):
+    print(i, end='#')
+#=>(0, 'a')#(1, 'b')#(2, 'c')#(3, 'a')#(4, 'b')#(5, 'c')#(6, 'a')#
+
+
+#accumulate:对序列前所有项累计计算
+a=list(range(1,6))
+b=list(itertools.accumulate(a))
+c=list(itertools.accumulate(a,func=operator.mul))
+print(a,b,c)
+#=>[1, 2, 3, 4, 5],[1, 3, 6, 10, 15],[1, 2, 6, 24, 120]
+
+
+#product(iter1,iter2, ... iterN, [repeat=1]);创建一个迭代器，生成表示item1，
+# item2等中的项目的笛卡尔积的元组，repeat是一个关键字参数，指定重复生成序列的次数
+a=itertools.product('ABCD', 'xy')
+print(list(a))
+#=>[('A', 'x'), ('A', 'y'), ('B', 'x'), ('B', 'y'), ('C', 'x'), ('C', 'y'), ('D', 'x'), ('D', 'y')]
+a=itertools.product(range(2), repeat=3)
+print(list(a))
+#=>[(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)]
+a=list(itertools.product('AB', 'xy', repeat=2))
+print(len(a),a)
+#=>16 [('A', 'x', 'A', 'x'), ('A', 'x', 'A', 'y'), ('A', 'x', 'B', 'x'), ('A', 'x', 'B', 'y'),
+#  ('A', 'y', 'A', 'x'), ('A', 'y', 'A', 'y'), ('A', 'y', 'B', 'x'), ('A', 'y', 'B', 'y'),
+# ('B', 'x', 'A', 'x'), ('B', 'x', 'A', 'y'), ('B', 'x', 'B', 'x'), ('B', 'x', 'B', 'y'),
+# ('B', 'y', 'A', 'x'), ('B', 'y', 'A', 'y'), ('B', 'y', 'B', 'x'), ('B', 'y', 'B', 'y')]
+
+
+#permutations(iterable, r=None)返回长度为r的所有可能的组合，每个项看做不同项，即使值相等
+a=list(itertools.permutations('aba'))
+print(len(a),a)
+#=>6 [('a', 'b', 'a'), ('a', 'a', 'b'), ('b', 'a', 'a'), ('b', 'a', 'a'), ('a', 'a', 'b'), ('a', 'b', 'a')]
+a=list(itertools.permutations(('a','b','c'),2))
+print(len(a),a)
+#=>6 [('a', 'b'), ('a', 'c'), ('b', 'a'), ('b', 'c'), ('c', 'a'), ('c', 'b')]
+def queen8(x):       #八皇后问题的一种写法
+    p=itertools.permutations(range(x))
+    for i in p:
+        if x==len(set((i[j]+j for j in i))) and x==len(set((i[j]-j for j in i))):
+            print(i)
+            for a in i:
+                z=['o']*x
+                z[a]='x'
+                print(''.join(z))
+
+
+#groupby可以自动将一个序列分组，相同相邻的元素会归为一组，形成一个新的生成器
+a=itertools.groupby('aasssa')
+for key, group in a:
+    print(key,'-->',list(group),end='   ')
+#=>a --> ['a', 'a']   s --> ['s', 's', 's']   a --> ['a']
+
+
+#combinations(iterable, r) 返回一个iterator，提供iterable中所有元素可能组合的r元组。
+# 每个元组中的元素保持与iterable返回的顺序相同。
+#combinations_with_replacement(iterable, r)函数放宽了一个不同的约束：
+# 元素可以在单个元组中重复，即可以出现aa/bb/cc/dd等组合
+a=list(itertools.combinations('abcd',r=3))
+print(len(a),a)
+#=>4 [('a', 'b', 'c'), ('a', 'b', 'd'), ('a', 'c', 'd'), ('b', 'c', 'd')]
+a=list(itertools.combinations_with_replacement(('a','b','c'),2))
+print(len(a),a)
+#=>6 [('a', 'a'), ('a', 'b'), ('a', 'c'), ('b', 'b'), ('b', 'c'), ('c', 'c')]
+
