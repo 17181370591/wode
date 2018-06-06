3da7cca5fafb2943a1398d0fa4ab01aaf3fd492e
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-06 22:44:15 +0800

    Update and rename 1.py to 西刺获取代理ip并测试.py

--- 西刺获取代理ip并测试.py
+++ 西刺获取代理ip并测试.py
@@ -1,5 +1,8 @@
 import requests,fake_useragent as fu,time,re,json
 from lxml import etree
+
+#测试ip，i是获取的ip的（地址，端口）元组，设置verify=True后能访问https页面，
+#设置timeout=5是不因为不想收集速度太慢的ip，proxies=p用来测试ip
 def checkip(i):
     p={'http':'http://{}:{}'.format(i[0],i[1]),\
        'https':'https://{}:{}'.format(i[0],i[1])}
@@ -15,8 +18,11 @@ def checkip(i):
         print(e)
 
 #每次get/post都要加headers，不然该次会被墙
+
+#随机获得请求头
 ua=fu.UserAgent()
 h=ua.random
+
 se=requests.session()
 print(h)
 nn=r'http://www.xicidaili.com/nn'
