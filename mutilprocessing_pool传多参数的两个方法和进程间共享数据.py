4ed3d3b78eb7da212c52d8e31f54fa43a6608805
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-07 14:50:37 +0800

    Update and rename 3.py to mutilprocessing_pool传多参数的两个方法和进程间共享数据.py

--- mutilprocessing_pool传多参数的两个方法和进程间共享数据.py
+++ mutilprocessing_pool传多参数的两个方法和进程间共享数据.py
@@ -3,29 +3,28 @@ from multiprocessing import Process, Manager,Pool
 import time
 import random
 
-from functools import partial
-
 def kkk(a_list, number):
     for i in range(10):
         a_list.append(i)
-#        time.sleep(random.randrange(2))
         print('这是进程{} {}'.format(number, a_list))
     print('这是进程{} {}'.format(number, a_list))
 
-def kk(a,b):
-    print(a+b)
-
-
 if __name__ == '__main__':
     
+    #manager用来在进程之间传递/共享参数
     manager = Manager()
     a_list = manager.list()
+    #如果a_list等于普通的列表，会发现数据没有共享
     #a_list = []
-    p = Pool(2)
     
+    p = Pool(2)
+    #方法1：使用starmap调用函数并传递多参数
     xy=zip((a_list,a_list),range(2))
     p.starmap(kkk,xy)
+    
     '''
+    #方法2：使用偏函数固定一个参数，然后用普通方法传另一个参数
+    from functools import partial
     kk2=partial(kkk,number=3)
     p.map(kk2,(a_list,a_list))
     '''
