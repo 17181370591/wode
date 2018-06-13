0585829d9f93b51cf09a5b72a1e434758c1f04d8
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-13 19:59:57 +0800

    Update and rename 4.py to itchat微信定期爬取并发送.py

---
+++ itchat微信定期爬取并发送.py
@@ -0,0 +1,23 @@
+#https://www.cnblogs.com/ouyangping/p/8453920.html
+#已经会使用登录登出，发文字文件图片信息，群发，获取朋友列表，公众号列表
+
+import itchat as wx, time,requests,fake_useragent as fu
+from itchat.content import *
+from lxml import etree
+
+#不登出的话，重复auto_login会被无视
+wx.auto_login(True)
+#wx.login()
+
+ua=fu.UserAgent()
+f=wx.search_friends(name='17077440160')[0]
+while True:
+    try:
+        h={'user-agent':ua.random}
+        r=requests.get('https://www.smzdm.com/',headers=h)
+        r=etree.HTML(r.text).xpath('//h5')
+        s=r[0].xpath('string(.)')               
+    except Exception as e:
+        s=e
+    f.send_msg(s)  
+    time.sleep(600)
