09bdd11ac191b32a4085d7b2feb1f1ea0d9ae9dc
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-22 20:07:00 +0800

    Update and rename 2.py to RSA加密解密.py

--- RSA加密解密.py
+++ RSA加密解密.py
@@ -6,18 +6,22 @@ from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5 
 
+#生成一组随机数
 rg=Random.new().read
 
 #1024表示密匙是1024位，2**1024得到1个309位的10进制数，因为这个原因rsa.n正好109位？
-rsa=RSA.generate(1024,rg)
-grsa=rsa.publickey()
+rsa=RSA.generate(1024,rg)		#用随机数生成rsa（？）
+grsa=rsa.publickey()			 
 
-ss=rsa.exportKey()
-gs=grsa.exportKey()
+ss=rsa.exportKey()			 #生成私匙
+gs=grsa.exportKey()			 #生成公钥
 
-rsa=RSA.importKey(ss)
+rsa=RSA.importKey(ss)			 #钥匙返回生成rsa（？）
 grsa=RSA.importKey(gs)
 
+
+#直接用公rsa对信息m加密，被加密的对象是bytes，base64编码后传输；
+#传输的数据用base64解码后，用私匙解密，得到m
 def ff(m):
     cis=PKCS1_v1_5.new(rsa)
     #cis=PKCS1_v1_5.new(RSA.importKey(ss))
@@ -29,12 +33,17 @@ def ff(m):
     c=base64.b64decode(d)
     print(cis.decrypt(c,'e'),type(c))
 
+
+#信息j长度较大时无法一次加密，所以用le表示最大解密长度，分割j进行加密
+#ll用来存储每一段的加密值，用b''.join合并ll生成一个长bytes返回
 def f(j,le):
     ll=[]
     for i in range(0,len(j),le):
 	ll.append(gcis.encrypt(j[i:i+le].encode()))
     return b''.join(ll)
 
+#f生成的bytes较长时无法一次解密，所以用le表示最大解密长度，进行分割解密
+#1024位密匙最大长度默认是128
 def g(a,le=128):
     ll=b''  
     for i in range(0,len(a),le):
