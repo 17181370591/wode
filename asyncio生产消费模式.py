41e53c8fb0162312e2f6088efe846144e42e996e
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-25 07:26:47 +0800

    Update and rename 1.py to asyncio生产消费模式.py

--- asyncio生产消费模式.py
+++ asyncio生产消费模式.py
@@ -1,4 +1,34 @@
+
+
+'''
+#原写法
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
+'''
+
+#换成asyncio的写法，下面添加的sleep全部是为了与单线程作比较用，不用添加
+
 import asyncio,time
+
 async def consumer(n):
     if not n:
        return
