a89e6f250dadad60ac2750ca36caa18e9e643415
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-06-25 21:17:24 +0800

    Update multiprocessing_Poor_0.py

--- multiprocessing_Poor_0.py
+++ multiprocessing_Poor_0.py
@@ -1,31 +1,39 @@
+
 #https://blog.csdn.net/seetheworld518/article/details/49639651
+
 import time
-from multiprocessing import Pool
+from multiprocessing import Pool as P1
+from multiprocessing。dummy import Pool as P2
 def run(fn):
   #fn: 函数参数是数据列表的一个元素
   time.sleep(1)
+  print(fn*fn)
   return fn*fn
 
 if __name__ == "__main__":
   testFL = [1,2,3,4,5,6]  
-  print ('shunxu:') #顺序执行(也就是串行执行，单进程)
-  s = time.time()
-  for fn in testFL:
-    run(fn)
-
-  e1 = time.time()
-  print ("顺序执行时间：", int(e1 - s))
-
   print ('concurrent:') #创建多个进程，并行执行
-  pool = Pool(5)  #创建拥有5个进程数量的进程池
+  pool = P1(5)  #创建拥有5个进程数量的进程池
+  #pool=P2(5)
   #testFL:要处理的数据列表，run：处理testFL列表中数据的函数
   
-  rl =pool.map(run, testFL) 
-  #上面这句也可以写成r1=[ pool1.apply_async(run,(i,)) for i in testFL ]
-  #pool1.apply_async不阻塞，pool1.apply阻塞
   
+  
+  #第一种获取返回值的方法，得到的是值
+  rl =pool.map(run, testFL) 
   pool.close()#关闭进程池，不再接受新的进程
   pool.join()#主进程阻塞等待子进程的退出
-  e2 = time.time()
-  print ("并行执行时间：", int(e2-e1))
-  print (rl)
+  print (r1)
+  for  i in r1:
+    print(i)
+    
+  #第二种获取返回值的方法，得到的是applyresult对象
+  '''
+  r1=[ pool.apply_async(run,(i,)) for i in testFL ]
+  #pool1.apply_async不阻塞，pool1.apply阻塞  
+  pool.close()#关闭进程池，不再接受新的进程
+  pool.join()#主进程阻塞等待子进程的退出
+  print (r1)
+  for  i in r1:
+    print(i.get())
+  '''
