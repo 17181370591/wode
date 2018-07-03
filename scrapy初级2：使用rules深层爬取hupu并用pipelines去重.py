ee9e03ed7838daf4c1a112bc4eaee88f79a9e1f8
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-03 22:29:45 +0800

    Update scrapy初级2：使用rules深层爬取hupu并用pipelines去重.py

--- scrapy初级2：使用rules深层爬取hupu并用pipelines去重.py
+++ scrapy初级2：使用rules深层爬取hupu并用pipelines去重.py
@@ -33,6 +33,16 @@ class MySpider(CrawlSpider):            #这里不能写scrapy.Spider
 ============================================================================
 #pipelines.py，用来处理spider yield的items
 #打开对应的redis集合，如果username的长度大于7且没有保存过，则保存
+'''
+为了启用一个Item Pipeline组件，你必须将它的类添加到 ITEM_PIPELINES 配置，就像下面这个例子:
+ITEM_PIPELINES = {
+    'myproject.pipelines.PricePipeline': 300,
+    'myproject.pipelines.JsonWriterPipeline': 800,
+}
+分配给每个类的整型值，确定了他们运行的顺序，item按数字从低到高的顺序，通过pipeline，
+通常将这些数字定义在0-1000范围内。
+'''
+
 
 import scrapy,logging,time,datetime
 from redis import Redis
