ac8b5da6d6cd117370bd5de30dc5967064872698
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-05 20:08:28 +0800

    Update scrapy初级6.0：使用cookies登录hupu未完成.py

--- scrapy初级6.0：使用cookies登录hupu未完成.py
+++ scrapy初级6.0：使用cookies登录hupu未完成.py
@@ -1,36 +1,23 @@
 #用已经登录成功的cookies伪装登录，d是cookies字典
 #直接在start_requests的scrapy.Request里设置好cookies就可以登录了，
-#不知道是虎扑检测不严厉还是怎么回事，在requests里cookies一定要在session访问一个网页后，
-#使用session.cookies.update更新cookies才能使用。
+
 
 #response.request.headers.getlist('Cookie')[0].decode()可以得到cookies的类似url链接的文本（str），
 #reco接收这个文本，并进行修改，返回的cs1是长度为0的list，内容是修改后的cookies的类似url链接的文本(bytes)，
-#可以直接传给response.request.headers.setlist，用来更新cookies
-#然而不知道虎扑的检测机制，不知道这么做能不能有效的更改cookies，因为不用reco也会成功
 
+
+#settings.py里有COOKIES_ENABLED配置，默认为True，这种情况下，
+#在spider.py用cookies伪装登录后会访问其他页面scrapy会自动处理cookies，设为false会访问失败。
+#但如果在settings.py设置cookies需要设置为false，详情见初级6.2
+
+
+====================================================================================
+
+#spider.py
 import scrapy
-
-def reco(Cookie):
-        c=Cookie.split(';')
-        dd={}
-        for i in c:
-            x=i.split('=',1)
-            dd[x[0]]=x[1]
-        #dd.update({'us':'你是二笔吗'})
-        for i in dd:
-            dd[i]='1'
-        print(dd)
-        cs=''
-        for i in dd.keys():
-            cs+=i.strip()+'='+dd[i].strip()+';'
-        cs=cs[:-1].encode()
-        cs1=[]
-        cs1.append(cs)
-        return cs1
-    
 class MySpider(scrapy.Spider):
     name = 'Hupu'
-    start_urls = [r'https://bbs.hupu.com/topic-12',r'https://bbs.hupu.com/topic-13']
+    start_urls = [r'https://bbs.hupu.com/topic-12']
     d={'_fmdata':'kYqIQs9LTYz5oRjOMdPb5iB6bclI6hqFswD5tXPSkD%2FcaKyCqSGi5cytdTcXbHzuqikmcDIabpykxIC7e8anCwZ3DY1NSSBrF0jE1yBSfks%3D',
     'us':'9b1005328020120a3ee8a32ce9f717db590c7cf302f799bdcf9854a6985ccb916dfeac9c343894e00b5e7b01847f2fa228d42d409c2854944882bcfec3a5e1c4',
    'u':'35048460|6Zi/5Y2h6KW/MQ==|4015|a624d0ae11f14f7df55e50e48f082772|11f14f7df55e50e4|aHVwdV8yZjQxZjJiM2YwNTg1ZTQw',
@@ -41,16 +28,33 @@ class MySpider(scrapy.Spider):
    }
     
     def parse(self, response):
-        print(len(response.text))   #response.body
+        print(len(response.text))  
+        print(response.text[:1111])
         
-        Cookie = response.request.headers.getlist('Cookie')[0].decode()
-        response.request.headers.setlist('Cookie',reco(Cookie))  
-
-        yield scrapy.Request(self.start_urls[1],callback=self.parse)
-
-
     def start_requests(self):
-        # 带着cookie向网站服务器发请求，表明我们是一个已登录的用户
         yield scrapy.Request(self.start_urls[0],
                              callback=self.parse,
                              cookies=self.d)
+
+
+====================================================================================
+
+#settings.py
+        
+# -*- coding: utf-8 -*-
+import fake_useragent as fu
+
+BOT_NAME = 'Hupu'
+
+SPIDER_MODULES = ['Hupu.spiders']
+NEWSPIDER_MODULE = 'Hupu.spiders'
+
+ua=fu.UserAgent()
+ROBOTSTXT_OBEY = False
+
+#COOKIES_ENABLED = True                         #默认为True，设置成false不会自动处理cookies
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'USER-AGENT ': ua.random ,
+
+}
