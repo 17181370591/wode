2a9bbee272c130cffc77def34c39a7fbbf0bdd15
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-05 18:58:55 +0800

    Update and rename gi.py to scrapy初级实战1：链接爬取一个交易.py

--- scrapy初级实战1：链接爬取一个交易.py
+++ scrapy初级实战1：链接爬取一个交易.py
@@ -1,3 +1,5 @@
+#注意rules的回调函数和类函数的名字不能用parse，不然抓不到数据，原因不明
+
 from scrapy import Spider, Request, FormRequest
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,6 +9,7 @@ from redis import Redis
 
 red=Redis(password='asd123')
 logger = logging.getLogger(__name__)
+
 class LJSpider(CrawlSpider):
     i=0
     name = "Hupu"
@@ -14,15 +17,13 @@ class LJSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(
-allow=(r'/chengjiao/[a-z]{4,}\d{0,2}/$',)),
+            allow=(r'/chengjiao/[a-z]{4,}\d{0,2}/$',)),
             # https://nj.lianjia.com/chengjiao/jianye/
-             callback='parse', follow=1),
+             callback='parse1', follow=1),
     )
 
 
-    def parse(self, response):
-        
-        
+    def parse1(self, response):       
         a=response.xpath('//ul[@class="listContent"]/li')[0]
         b=a.xpath('string(div/div[@class="title"])').extract()[0]
         red.lpush('x',response.url)
