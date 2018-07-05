1d04efc838c427e789750ed938515df073530b8c
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-05 12:13:45 +0800

    Update scrapy初级6.1：dont_filter=True不去重抓取.py

--- scrapy初级6.1：dont_filter=True不去重抓取.py
+++ scrapy初级6.1：dont_filter=True不去重抓取.py
@@ -1,6 +1,9 @@
 #post_login访问'https://my.suning.com'时，抓包可以发现回访问两次（直接访问一次，重定向一次），
 #由于第二次才会抓取数据，第一次访问后第二次访问会被过滤，所以抓取不到数据，设置dont_filter=True即可
 
+#重定向有可能会遇到类似问题，所以最好都加dont_filter=True？？
+#登陆成功后，访问我的订单，我的设置都会自己附上cookies，所以不知道meta设置cookiejar有什么用
+
 from scrapy import Spider, Request, FormRequest
 
 class GithubLoginSpider(Spider):
@@ -28,6 +31,7 @@ class GithubLoginSpider(Spider):
     def post_login(self, response):
         print('登录'*8,response.text)
         return Request('https://my.suning.com',
+                       #meta={'cookiejar': response.meta['cookiejar']},
                        callback=self.pa,dont_filter=True)       #不设置dont_filter=True抓不到数据
     def pa(self, response):      
         with open('1.txt', 'w',encoding='utf8')as f:
