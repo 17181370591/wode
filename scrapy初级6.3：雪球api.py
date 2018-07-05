f8a48c3c7f28ac80d1e94b7be1a0c5f14c56f549
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-05 12:28:33 +0800

    Update and rename gi.py to scrapy初级6.3：雪球api.py

--- scrapy初级6.3：雪球api.py
+++ scrapy初级6.3：雪球api.py
@@ -1,9 +1,10 @@
-from scrapy import Spider, Request, FormRequest
+#需要县访问雪球网，获取到cookies才能访问api，
+#依旧不用设置meta，两个请求都要设置headers，所以最好在settings里设置
 
+from scrapy import Spider, Request, FormRequest
 
 class GithubLoginSpider(Spider):
     name = "gi"
-    # post登入的必须要的头字段
     h = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36",
     }
@@ -12,9 +13,9 @@ class GithubLoginSpider(Spider):
 
     def start_requests(self):
         return [Request("https://xueqiu.com/",
-                        meta = {'cookiejar' : 11},
+                        #meta = {'cookiejar' : 11},
                          headers=self.h,
-                        callback = self.post_login)]  #添加了meta
+                        callback = self.post_login)]  
 
     #FormRequeset出问题了
     def post_login(self, response):
@@ -22,12 +23,11 @@ class GithubLoginSpider(Spider):
         return Request('https://xueqiu.com/stock/f10/finmainindex.json?symbol=SZ000001&page=1&size=1',
                        callback=self.pa,
                        headers=self.h,
-                       meta={'cookiejar': response.meta['cookiejar']},
+                       #meta={'cookiejar': response.meta['cookiejar']},
                        dont_filter=True
                        )
     def pa(self, response):
         print(response.text)
-        #'''
         with open('1.txt', 'w',encoding='utf8')as f:
             f.write(response.text)
-        #'''
+        
