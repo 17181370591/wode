a737d1bdc874fc6477975f7ba5654f8d3f281db4
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-05 19:02:46 +0800

    Update and rename scrapy初级实战1：链接爬取一个交易.py to scrapy初级实战1：链家爬取一个交易.py

--- scrapy初级实战1：链家爬取一个交易.py
+++ scrapy初级实战1：链家爬取一个交易.py
@@ -1,4 +1,9 @@
 #注意rules的回调函数和类函数的名字不能用parse，不然抓不到数据，原因不明
+#rule里的正则获取地区/街道的链接，parse1打印第一个交易信息，
+#使用follow=True爬取南京所有地区和街道。
+#从redis可以发现，有两个地区没有交易记录，一共有11个小区，
+#所以y有96个值，87个不重复的，
+#x有96个不重复值，11个地区url，85个街道url
 
 from scrapy import Spider, Request, FormRequest
 import scrapy
