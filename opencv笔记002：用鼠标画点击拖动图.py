7c3083e9329eb12f28f832bc420f67d01b6ac1ef
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-06 20:53:19 +0800

    Update and rename 1.py to opencv笔记002：用鼠标画点击拖动图.py

--- opencv笔记002：用鼠标画点击拖动图.py
+++ opencv笔记002：用鼠标画点击拖动图.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
+drawing = False     #画图模式，防止鼠标经过就画图
+mode = True         #画矩形模式，按m切换，可以切换成画圆模式
+ix,iy = -1,-1       #随便给个初始值
 
-# mouse callback function
+
+
 def draw_circle(event,x,y,flags,param):
     global ix,iy,drawing,mode
 
@@ -27,16 +28,16 @@ def draw_circle(event,x,y,flags,param):
             cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),1)
         else:
             cv2.circle(img,(x,y),5,(0,0,255),-1)
-# Next we have to bind this mouse callback function to OpenCV # # window. In the main loop, we should set a keyboard binding for 
-# key ‘m’ to toggle between rectangle and circle.
+
+            
 img = np.zeros((512,512,3), np.uint8)
-cv2.namedWindow('image')
+cv2.namedWindow('image')                      #先创建窗口
 cv2.setMouseCallback('image',draw_circle)
 
 while(1):
     cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('m'): # 切换模式
+    k = cv2.waitKey(1) 
+    if k == ord('m'):                    # 切换模式
         mode = not mode
     elif k == 27:
         break
